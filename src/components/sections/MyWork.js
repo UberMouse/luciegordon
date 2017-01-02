339b055569a17a9b8ba17f2fcb3a6a262a8ee67d
Taylor Lodge <taylor@boost.co.nz>
2017-01-02 23:44:00 +1300

    Get Modals functioning as a PoC

--- src/components/sections/MyWork.js
+++ src/components/sections/MyWork.js
@@ -31,10 +31,23 @@ class PortfolioItem extends React.Component {
 
         <Modal
           isOpen={modalOpen}
-          className="MyWorker--modal"
-          overlayClassName="MyWork--modal-overlay"
+          contentLabel="portfolio-modal"
         >
-          <h1>{this.props.hoverText}</h1>
+          <span
+            className="Modal--close"
+            onClick={() => this.setState({modalOpen: false})}
+          >X</span>
+          <Row isColumn>
+            <h2 className="Modal--header">
+              {this.props.hoverText}<span className="pink">.</span>
+            </h2>
+          </Row>
+          <Row isColumn>
+            <p className="Modal--description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lacinia lacinia magna imperdiet dapibus. Curabitur vitae justo ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget tincidunt felis, eu ullamcorper velit. Suspendisse dui est, lacinia et rhoncus vitae, vehicula viverra arcu. Praesent ligula sem, lobortis sit amet justo non, hendrerit accumsan elit. Praesent porttitor efficitur elit sit amet tincidunt. Maecenas vel velit ac sem placerat malesuada vitae a risus. Sed pellentesque rutrum ligula ut vulputate. Suspendisse potenti. In mollis imperdiet dolor quis tincidunt.</p>
+          </Row>
+          <Row isColumn className="align-center">
+            <img src="http://placehold.it/650x650" className="Modal--image" />
+          </Row>
         </Modal>
       </Column>
     );
