0e572768b9d11b8c99610864370a9941d5a0a2f6
Taylor Lodge <taylor@boost.co.nz>
2017-01-05 17:15:43 +1300

    Updates

---
+++ js/scrollmagic-controllers.js
@@ -0,0 +1,18 @@
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// change behaviour of controller to animate scroll instead of jump
+controller.scrollTo(function (newpos) {
+  TweenMax.to(window, 0.5, {scrollTo: {y: newpos}});
+});
+
+//  bind scroll to anchor links
+$(document).on("click", "a[href^='#']", function (e) {
+  var id = $(this).attr("href");
+  if ($(id).length > 0) {
+    e.preventDefault();
+
+    // trigger scroll
+    controller.scrollTo(id);
+  }
+});
